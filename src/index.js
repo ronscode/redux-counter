8b0cba8c45cbf4b433cd7f1116c01531c58a580a
Ron Cagle <ron@roncagle.com>
2020-05-29 14:43:33 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import { createStore } from 'redux';
+import { connect, Provider } from 'react-redux';
+
+const initialState = {
+  count: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  return state;
+}
+
+import './styles.scss';
+
+class Counter extends Component {
+  render() {
+    return (
+      <main className="Counter">
+        <p className="count">0</p>
+        <section className="controls">
+          <button>Increment</button>
+          <button>Decrement</button>
+          <button>Reset</button>
+        </section>
+      </main>
+    );
+  }
+}
+
+render(<Counter />, document.getElementById('root'));

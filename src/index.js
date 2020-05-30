52004844e6d7e0bddbda5086df70ca583866203c
Ron Cagle <ron@roncagle.com>
2020-05-30 08:50:02 -0400

    Connecting Redux to React

--- src/index.js
+++ src/index.js
@@ -8,9 +8,24 @@ const initialState = {
   count: 0,
 };
 
+const INCREMENT = 'INCREMENT';
+
+const incrementValue = () => ({
+  type: INCREMENT,
+});
+
 const reducer = (state = initialState, action) => {
+  if (action.type === INCREMENT) {
+    return {
+      count: state.count + 1,
+    }
+  }
+
+
   return state;
-}
+};
+
+const store = createStore(reducer);
 
 import './styles.scss';
 
